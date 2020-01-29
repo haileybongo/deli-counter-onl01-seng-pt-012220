43b1ce6b3c5695d194c657bf9e1fae4f99433803
Hailey Bongo <haileyebongo@gmail.com>
2020-01-29 21:44:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,10 @@ def line(array)
     puts "The line is currently empty."
   else
     counter = 1 
+    line_array = []
     array.each do |name|
-     
+     line_array << "#{counter}. #{name} "
+   end
+   puts "The line is currently: line
      
     
